a8f1b063eeb22e8437aa88c6c8ac3ece19629110
Vaibhav <vaibhavmittal99@gmail.com>
2018-05-27 23:48:04 +0530

    basicInfo

---
+++ app/src/main/java/com/example/vaibhavmitaliitian/evcards/BasicInfo.java
@@ -0,0 +1,9 @@
+package com.example.vaibhavmitaliitian.evcards;
+
+import java.util.ArrayList;
+
+public class BasicInfo {
+    String name,organisationName,designation,description;
+    ArrayList<String> phone,add,officeAdd,email,landline;
+
+}
